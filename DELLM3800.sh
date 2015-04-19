91dba6a3dae10cdc512aac14195d72a5bf8e8041
Austin James Andres <AustinSMU@users.noreply.github.com>
2015-04-19 18:10:51 -0500

    Update DELLM3800.sh

--- DELLM3800.sh
+++ DELLM3800.sh
@@ -227,6 +227,7 @@ patch_iokit()
 		sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 		sudo codesign -f -s - /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
 		echo "         Patched"
+		;;
 		"29d7632362b2fa4993156717671a5642")
 		echo "         --> Yosemite 10.10.3 IOKit (${GREEN}unpatched${OFF})"
 		sudo perl -i.bak -pe 's|\xB8\x01\x00\x00\x00\xF6\xC1\x01\x0F\x85|\x33\xC0\x90\x90\x90\x90\x90\x90\x90\xE9|sg' /System/Library/Frameworks/IOKit.framework/Versions/Current/IOKit
